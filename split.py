0c010f086d39d962d87a038d8b7f77c4db1ef173
tateiwa <inarizuuuushi@gmail.com>
2017-01-08 20:39:36 +0900

    Update split.py

--- split.py
+++ split.py
@@ -3,39 +3,47 @@ from sys import argv, stdin
 
 __doc__ = """
 Usage:
-    split [-s] str
+    split char
+    split [-n] number
     split [-h | --help]
 
 Options:
-    -s            Strip lines.
+    -n <number>   numberでstripする。  
     -h --help     Show this screen and exit.
 
 Note:
-    標準入力をstrでsplitする
+    標準入力をsplitする。
+    ex)
+        abc.py | split .     ->  abc py
+        abc.py | split -n 2  ->  ab c.py
 """
 
-STRIP = False
+NUMBER = False
 
 def usage():
     print(__doc__)
     exit()
 
-def main(_str):
+def main(key):
     for row in stdin.readlines():
-        if STRIP:
-            row = row.strip()
+        row = row.strip()
+        if NUMBER:
+            key = int(key)
+            try: row = [row[:key], row[key:]]
+            except: row = [row]
         else:
-            row = row.replace('\n', '')
-        print(' '.join(row.split(_str)))
+            row = row.split(key)
+        print(' '.join(row))
+
 
 if __name__ == '__main__':
     for v in argv[1:]:
         if v in ['-h', '--help']:
             usage()
-        if v in ['-s']:
-            STRIP = True
+        if v in ['-n']:
+            NUMBER = True
             argv.remove(v)
     if len(argv) == 2:
-        main(_str=argv[1])
+        main(key=argv[1])
     else:
         usage()
