de024f0f9e2a703291013ad4f7b26f6bad1c5bf5
立岩斉明 <inarizuuuushi@gmail.com>
2017-02-03 22:55:24 +0900

    add file

---
+++ count.py
@@ -0,0 +1,30 @@
+#!/usr/local/bin/python3
+from sys import argv, stdin
+from collections import Counter
+
+__doc__ = """
+Usage:
+    count [-h | --help]
+
+Options:
+    -h --help       Show this screen and exit.
+
+Note:
+    標準入力をカウントする
+"""
+
+def usage():
+    print(__doc__)
+    exit()
+
+def main():
+    data = [row.strip() for row in stdin.readlines()]
+    data = Counter(data)
+    for key in data:
+        print('{}: {}'.format(key, data[key]))
+
+if __name__ == '__main__':
+    for v in argv[1:]:
+        if v in ['-h', '--help']:
+            usage()
+    main()
