c6383703e70820fc62d45111de1815c1f4638f56
立岩斉明 <n-tateiwa@math.kyushu-u.ac.jp>
2020-01-12 17:45:06 +0900

    update pywhile

--- pywhile.py
+++ pywhile.py
@@ -1,37 +1,63 @@
 #!/usr/local/bin/python3
 from sys import argv, stdin
 import subprocess as sp
+from multiprocessing import Pool
 
 __doc__ = """
 Usage:
-	pywhile COMMAND
-	pywhile [-h | --help]
+    pywhile COMMAND
+    pywhile [-h | --help]
 
 Options:
     -h --help     Show this screen and exit.
+    -p --parallel Run parallel
 
 Note:
-	COMMADN 標準入力を次々と実行
-	ex)
-	    ls | grep test | pywhile rm
-	    ls | grep test | pywhile rm -r
+    COMMADN 標準入力を次々と実行
+    ex)
+        ls | pywhile rm
+        ls | pywhile rm -r
+        ls | pywhile "python script.py #f"
+        ls | pywhile "python script.py #f" -p
 """
 
+PARALLEL = False
+
 def usage():
-	print(__doc__)
-	exit()
+    print(__doc__)
+    exit()
+
+
+def gen_command(com, line):
+    # com: list -> str
+    command = ' '.join(com)
+    if "#f" in command:
+        command = command.replace('#f', line.strip(), -1)
+    else:
+        command = command + ' ' + line.strip()
+    # com: str -> list
+    command = command.split()
+    return command
 
 
 def main(com):
-	for row in stdin.readlines():
-		sp.run(com + [row.strip()])
+    if not PARALLEL:
+        for line in stdin.readlines():
+            sp.run(gen_command(com, line))
+    else:
+        commands = [gen_command(com, line) for line in stdin.readlines()]
+        with Pool(processes=None) as pool:
+            pool.map(sp.run, commands)
 
 
 if __name__ == '__main__':
-	for v in argv[1:]:
-		if v in ['-h', '--help']:
-			usage()
-	if len(argv) != 1:
-		main(com=argv[1:])
-	else:
-		usage()
+    for v in argv[1:]:
+        if v in ['-h', '--help']:
+            usage()
+        if v in ['-p', '--parallel']:
+            PARALLEL = True
+            argv.remove(v)
+    if len(argv) != 1:
+        main(com=argv[1:])
+    else:
+        usage()
