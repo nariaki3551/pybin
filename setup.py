e21172552b0258886801d47f0a056c00a1ab233f
立岩斉明 <n-tateiwa@math.kyushu-u.ac.jp>
2020-01-24 21:43:18 +0900

    src, binでスクリプトとシンボリックファイルを分けて管理 / requirement.txtを作成 /  setupt.py  shebangが環境によって変更されるように改良

--- setup.py
+++ setup.py
@@ -1,66 +1,91 @@
-#!/usr/bin/env python3
+import os
+import argparse
 import subprocess as sp
-from sys import argv
 
-__doc__='''
-Usage:
-        python3 setup.py [--reset]
-
-Note:
-        install python packaage and
-        make symbolic links for all commands.
-'''
-
-scripts \
-    = ['T.py',
-       'pyadd.py',
-       'pycolumn.py',
-       'pycorr.py',
-       'pyjoin.py',
-       'pyline.py',
-       'pymax.py',
-       'pymean.py',
-       'pymin.py',
-       'pydiv.py',
-       'pysort.py',
-       'pyremove.py',
-       'pyreplace.py',
-       'pyreverse.py',
-       'pysplit.py',
-       'pysum.py',
-       'pyvar.py',
-       'pywhile.py',
-       'pycount.py',
-       'pyif.py',
-       'pybin.py',
-       'pycolor.py'
-      ]
-
-def main():
-    try:
-      # install python package
-      sp.run(['pip3', 'install', 'numpy'])
-      sp.run(['pip3', 'install', 'fabric3'])
+SRC_DIR = './src'
+BIN_DIR = './bin'
+scripts = [
+    'T.py',         'pyadd.py',     'pycolumn.py',
+    'pycorr.py',    'pyjoin.py',    'pyline.py',
+    'pymax.py',     'pymean.py',    'pymin.py',
+    'pydiv.py',     'pysort.py',    'pyremove.py',
+    'pyreplace.py', 'pyreverse.py', 'pysplit.py',
+    'pysum.py',     'pyvar.py',     'pywhile.py',
+    'pycount.py',   'pyif.py',      'pybin.py',
+    'pycolor.py'
+]
 
 
-      # make symbolic links
-      for file in scripts:
-          print('chmod +x {}'.format(file))
-          sp.getoutput('chmod +x {}'.format(file))
-          print('ln -s {} {}'.format(file, file.replace('.py', '')))
-          sp.getoutput('ln -s {} {}'.format(file, file.replace('.py', '')))
-    except:
-      print('\n ERROR'); exit()
+def main(args):
+    rewrite_shebang(args)
+    make_symbolic(args)
+
+
+def rewrite_shebang(args):
+    shebang = '#!'+args.python_path
+    print('shebang:', shebang)
+    for pyfile in scripts:
+        pyfile_path = SRC_DIR + '/' + pyfile
+        sp.getoutput("sed '1d' " + pyfile_path)
+        sp.getoutput("sed -i '{}' {}".format(shebang, pyfile_path))
+
+
+def make_symbolic(args):
+    # make symbolic links
+    for pyfile in scripts:
+        name = pyfile.replace('.py', '')
+        pyfile_path = SRC_DIR + '/' + pyfile
+        bin_path = BIN_DIR + '/' + name
+        print('chmod +x {}'.format(pyfile_path))
+        sp.getoutput('chmod +x {}'.format(pyfile_path))
+        print('ln -s {} {}'.format(pyfile_path, bin_path))
+        sp.getoutput('ln -s {} {}'.format(pyfile_path, bin_path))
 
     print('\n🍣  ALL COMPLATE')
 
-def reset():
-    for file in scripts:
-        print('unlink {}'.format(file.replace('.py', '')))
-        sp.getoutput('unlink {}'.format(file.replace('.py', '')))    
+    description = [
+        'make symbolic in ./bin directory,',
+        '',
+        'please write in your ~/.bash_profile, ~/.bashrc or ~/.zshrc',
+        '',
+        '    export PATH={}:${{PATH}}'.format(os.getcwd()),
+        ''
+    ]
+    print('\n'.join(description))
+
+
+def clean():
+    for pyfile in scripts:
+        name = pyfile.replace('.py', '')
+        bin_path = BIN_DIR + '/' + name
+        print('unlink {}'.format(bin_path))
+        sp.getoutput('unlink {}'.format(bin_path))
+
+
+def argparser():
+    description='make symbolic links for all commands'
+    parser = argparse.ArgumentParser(
+        prog=__file__,
+        description=description
+    )
+    parser.add_argument(
+        '--python_path',
+        default=sp.getoutput('which python3'),
+        help='python path you use (default: which python3)'
+    )
+    parser.add_argument(
+        '--clean',
+        action='store_true',
+        help='clear binary files'
+    )
+    return parser
+
 
 if __name__ == '__main__':
-    if '--reset' in argv:
-      reset()
+    parser = argparser()
+    args = parser.parse_args()
+
+    if args.clean:
+        clean()
     else:
-      main()
+        main(args)
